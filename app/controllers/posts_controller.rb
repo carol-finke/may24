fed95ff98143bfa563803aa0497f7ae019cf2669
Carol Finke <carol.finke@kellogg.northwestern.edu>
2024-05-24 16:40:31 +0000

    in class updates

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+    before_action :current_user
+  
+    def index
+      @posts = Post.all
+    end
+    def create
+      @post = Post.new
+      @post["body"] = params["body"]
+      @post["user_id"] = session["user_id"]
+      @post.save
+      redirect_to "/posts"
+    
+    end 
+  
+  end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'marketing/page'
   # Resources
   resources "sessions"
   resources "users"
+  resources "posts"
   
   # Login/logout
   get("/login", { :controller => "sessions", :action => "new" })
   get("/logout", { :controller => "sessions", :action => "destroy" })
+  
 
   # Define the root route
   get("/", { :controller => "users", :action => "new" })
